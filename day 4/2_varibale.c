2fc482c409054c0be8be720c91904e62a657e436
unknown <saniyabanupkanakwad@gmail.com>
2025-11-20 09:42:01 +0530

    day4 code  pushed

---
+++ day 4/2_varibale.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<stdbool.h>
+int main()
+{
+    int age = 30;
+    printf("your age is:%d \n", age);
+    float marks = 500.50;
+    printf("your marks is:%f \n", marks);
+    char name = 'j';
+    printf("your name is:%c \n", name);
+    double pi=3.14;
+    printf("value of pi is:%lf \n",pi);
+    bool iswalking=true;
+    printf("is walking:%d \n",iswalking);
+}
